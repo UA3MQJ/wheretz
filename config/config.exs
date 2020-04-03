6490f28ec28e58172d16bbdbd46b528cd8435c02
UA3MQJ <ua3mqj@gmail.com>
2020-04-03 10:53:49 +0300

    fix again

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,4 @@
 use Mix.Config
+
+config :mnesia,
+  dir: './priv/mnesia_db' # use single quotes here !!!
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WhereTZ.MixProject do
     [
       app: :wheretz,
       description: "Time zone by geo coordinates lookup",
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
@@ -29,7 +29,7 @@ defmodule WhereTZ.MixProject do
   def application do
     [
       mod: {WhereTZ.Application, []},
-      extra_applications: [:logger]
+      extra_applications: [:logger, :mnesia]
     ]
   end
 
